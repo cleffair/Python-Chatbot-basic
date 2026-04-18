847f13d0098b1a7990da34f4f69e6e2352de5b30
cleffair <cleffework@gmail.com>
2026-04-18 20:16:14 +0530

    few more lines of code

--- new.py
+++ new.py
@@ -1,18 +1,29 @@
 # import all the things here first 
 import json
 import numpy
-import tensorflow 
-import pickle
+import tensorflow as tf
+import pickle as pk
 import random
 import nltk
 
-
 from nltk.stem import WordNetLemmatizer
 
-lemmatizer = WordNetLemmatizer()
+lemmatizer = WordNetLemmatizer
 
-# now we have to load the intents.json file 
-with open('intents.json') as file:
-    data = json.load(file)
+#loading our intentsjson file now 
 
-print(data)
+intents = json.loads(open('intents.json').read())
+
+words = []
+classes = []
+documents = [] 
+ignoreLetters = ['!', '?', '.', ',']
+
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        wordList = nltk.word_tokenize(pattern)
+        words.extend(wordList)
+        documents.append((wordlist, intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
